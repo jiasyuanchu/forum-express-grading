695ac1f5d908440f86c0b1427619de48bb07bd1b
jiasyuanchu <jiasyuanchu@gmail.com>
2023-05-30 15:50:02 +0800

    feat: add api error handler & delete restaurant api

--- middleware/error-handler.js
+++ middleware/error-handler.js
@@ -1,4 +1,5 @@
 module.exports = {
+  // 在畫面上用 flash message 來呈現
   generalErrorHandler (err, req, res, next) {
     if (err instanceof Error) {
       req.flash('error_messages', `${err.name}: ${err.message}`)
@@ -6,6 +7,21 @@ module.exports = {
       req.flash('error_messages', `${err}`)
     }
     res.redirect('back')
+    next(err)
+  },
+  //  API 版, 把錯誤訊息傳給前端
+  apiErrorHandler (err, req, res, next) {
+    if (err instanceof Error) {
+      res.status(err.status || 500).json({
+        status: 'error',
+        message: `${err.name}: ${err.message}`
+      })
+    } else {
+      res.status(500).json({
+        status: 'error',
+        message: `${err}`
+      })
+    }
     next(err)
   }
 }
